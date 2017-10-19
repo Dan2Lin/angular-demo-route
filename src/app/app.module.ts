e758093fbd716689414a618a692b3a12ea66c612
Dan2Lin <969990114@qq.com>
2017-10-19 18:01:04 +0800

    添加模块懒加载功能

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import {RouterModule, Routes} from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { NewsComponent } from './news/news.component';
-import { SolutionComponent } from './solution/solution.component';
-import { AboutComponent } from './about/about.component';
 import { SociatyComponent } from './sociaty/sociaty.component';
 import { HotnewsComponent } from './hotnews/hotnews.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren :'./home/home.module#HomeModule'
+  },
+  {
+    path: 'news',
+    component: NewsComponent,
+    children : [
+      {path: 'sociaty', component: SociatyComponent},
+      {path: 'hotnews', component: HotnewsComponent},
+      {path: '**', component: SociatyComponent},
+    ]
+  },
+  {
+    path: 'news/:nid',
+    component: NewsComponent
+  },
+  {
+    /*通配符:默认显示首页*/
+    path: '**',
+    component: WelcomeComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     NewsComponent,
-    SolutionComponent,
-    AboutComponent,
     SociatyComponent,
     HotnewsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
